2e720c80de8a32bb9a62847f6a272a6b2788b148
Yash Karanke <yash.rockersinfo@gmail.com>
2021-08-23 00:03:08 +0530

    setup apollo

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const BookList = (props) => {
+    return (
+        <div>
+            <ul id="bookList">
+                <li>Book Name</li>
+            </ul>
+        </div>
+    );
+};
